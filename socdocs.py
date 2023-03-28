07aeae1d91008bf191939d135395bab08b8272e4
Chris Surline <chris.surline@gmail.com>
2023-03-28 00:13:59 -0400

    Update socdocs.py

--- socdocs.py
+++ socdocs.py
@@ -1,5 +1,3 @@
-# 03-20-2023 - Chris Surline
-
 from datetime import date
 import configparser
 import argparse
@@ -7,6 +5,7 @@ import os
 import pathlib
 import vt
 import requests
+import re
 from constants import virustotal_api_key, metadefender_api_key
 
 def make_file(filepath, filename):
@@ -14,21 +13,28 @@ def make_file(filepath, filename):
     if not os.path.exists(newfile):
         open(newfile, 'x')
 
-def hash_search(filehash):
+def ioc_search(ioc, querytype):
 
     if virustotal_api_key:
         with vt.Client(virustotal_api_key) as client:
-            vtreport = client.get_json('/files/' + filehash)
-            vtreportfile = open(datepath + '/VirusTotal-report_' + filehash + '.txt', 'w')
+            match querytype:
+                case 'domain':
+                    queryvar = '/domains/'
+                case 'ip':
+                    queryvar = '/ip_addresses/'
+                case 'hash':
+                    queryvar = '/files/'
+            vtreport = client.get_json(queryvar + ioc)
+            vtreportfile = open(datepath + '/VirusTotal-report_' + ioc + '.txt', 'w')
             vtreportfile.write(str(vtreport))
             vtreportfile.close()
             print(vtreport)
 
     if metadefender_api_key:
-        mdapiquery = 'https://api.metadefender.com/v4/hash/' + filehash
+        mdapiquery = 'https://api.metadefender.com/v4/' + querytype + '/' + ioc
         mdheaders = {'apikey': metadefender_api_key}
         mdreport = requests.request("GET", mdapiquery, headers=mdheaders)
-        mdreportfile = open(datepath + '/MetaDefender-report_' + filehash + '.txt', 'w')
+        mdreportfile = open(datepath + '/MetaDefender-report_' + ioc + '.txt', 'w')
         mdreportfile.write(str(mdreport.text))
         mdreportfile.close()
         print(mdreport.text)
@@ -56,5 +62,12 @@ if __name__ == "__main__":
     if args.filename:
         make_file(datepath, '/' + args.filename)
 
-    if args.query:
-        hash_search(args.query)
+    # determine if query is hash, IP or url/domain
+    elif re.match("(?:http(?:s?)://)?(?:[\w]+\.)+[a-zA-Z]+(?::\d{1,5})?", args.query): # check if url/domain
+        # if it is a full URL strip it down to base domain
+        domain = re.search('^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)', args.query)
+        ioc_search(domain.group(1), 'domain')
+    elif re.match("^(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.(?!$)|$)){4}$", args.query): # check if IPv4 address
+        ioc_search(args.query, 'ip')
+    else: # if not everything else than try searching as a file hash 
+        ioc_search(args.query, 'hash')
