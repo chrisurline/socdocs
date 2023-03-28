1ca4c6dd7d662096dde48d92a25390048088d265
Chris Surline <chris.surline@gmail.com>
2023-03-28 09:08:41 -0400

    Delete constants.py

--- constants.py
+++
@@ -1,5 +0,0 @@
-# API Keys
-virustotal_api_key = '[VIRUSTOTAL_API_KEY_HERE]'
-metadefender_api_key = '[METADEFENDER_API_KEY_HERE]'
-# FileScan.io seems redundant at the moment just leaving here as placeholder 
-# filescanio_api_key = '[FILESCAN.IO_API_KEY_HERE]'
